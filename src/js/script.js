38ab567c38b3468e694dc53758db21e0b5be0bad
chiehaya <jakkiriko@yahoo.co.jp>
2023-10-10 22:33:39 +0900

    ２回目のコミット

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+// ハンバーガーメニュー
+const button = $(".js-button");
+const globalNav = $(".js-page-nav");
+const buttonBody = $(".js-button__body");
 
+button.on("click", function () {
+    buttonBody.toggleClass("is-active");
+    globalNav.toggleClass("is-show");
+});
 
 });
